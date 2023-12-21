79bdf3aadab815b929c4621d153570bcf874e33e
Obada-Yahya <obadayahya.an@gmail.com>
2023-12-21 10:56:58 +0200

    Added Custom Exceptions

---
+++ Domain/Exceptions/HotelNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions;
+
+public class HotelNotFoundException : Exception
+{
+    public HotelNotFoundException(Guid id)
+        : base($"The hotel with the ID = {id} was not found")
+    {
+        
+    }
+}
