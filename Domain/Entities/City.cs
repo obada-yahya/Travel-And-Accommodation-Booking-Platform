ab923e9a54bd5f00abfa6631cc343408308ee1ea
Obada-Yahya <obadayahya.an@gmail.com>
2023-12-21 09:39:41 +0200

    Added City,Owner,Hotel Entities

---
+++ Domain/Entities/City.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Entities;
+
+public class City
+{
+    public City(Guid id, string name, string countryName, string countryCode, string postOffice, IList<Hotel> hotels)
+    {
+        Id = id;
+        Name = name;
+        CountryName = countryName;
+        CountryCode = countryCode;
+        PostOffice = postOffice;
+        Hotels = hotels;
+    }
+    
+    public Guid Id { get; private set; }
+    public string Name{ get; private set; }
+    public string CountryName{ get; private set; }
+    public string CountryCode{ get; private set; }
+    public string PostOffice{ get; private set; }
+    public IList<Hotel> Hotels { get; private set; }
+}
---
+++ Domain/Entities/Owner.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Entities;
+
+public class Owner
+{
+    public Owner(Guid id, string firstName, string lastName, string email, string phoneNumber, IList<Hotel> hotels)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Hotels = hotels;
+    }
+    
+    public Guid Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public IList<Hotel> Hotels { get; private set; }
+    
+}
