d2ca9763d3adca0c48310f906d40c74f8fe8be50
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-02 07:22:37 +0200

    Updated CityRepository TestCases

--- TAABP.Tests/InfrastructureTests/RepositoriesTests/CityRepositoryTests.cs
+++ TAABP.Tests/InfrastructureTests/RepositoriesTests/CityRepositoryTests.cs
@@ -1,35 +1,29 @@
 ﻿using Domain.Entities;
+using FluentAssertions;
 using Infrastructure.Common.Persistence;
 using Infrastructure.Common.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using TAABP.Tests.InfrastructureTests.RepositoriesTests.TestData;
-using Xunit.Abstractions;
 
 namespace TAABP.Tests.InfrastructureTests.RepositoriesTests;
 
 public class CityRepositoryTests
 {
-    private readonly ITestOutputHelper _testOutputHelper;
     private readonly ApplicationDbContext _context;
 
-    public CityRepositoryTests(ITestOutputHelper testOutputHelper)
+    public CityRepositoryTests()
     {
-        _testOutputHelper = testOutputHelper;
-        // Should Comment OnConfiguring Method to work
-        var options = new DbContextOptionsBuilder()
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(Guid.NewGuid().ToString());
         _context = new ApplicationDbContext(options.Options);
     }
-
+    
     [Theory]
     [MemberData(nameof(CityRepositoryTestData.CitiesTestData),
         MemberType = typeof(CityRepositoryTestData))]
-    [Trait("Category", "Queries")]
-    public async Task GetAllAsync_ShouldReturnAllCities(List<City> expectedCities)
+    [Trait("Category", "City")]
+    public async Task GetAllAsync_ShouldReturnAllCities_WhenCitiesExist(List<City> expectedCities)
     {
-        // Arrange
-
-        // Act
         _context.Cities.AddRange(expectedCities);
 
         await _context.SaveChangesAsync();
@@ -37,65 +31,56 @@ public class CityRepositoryTests
         var sut = new CityRepository(_context);
 
         var fetchedCities = await sut.GetAllAsync();
-
-        // Assert 
-        Assert.Equal(expectedCities.Count, fetchedCities.Count());
+        
+        fetchedCities.Should().HaveCount(expectedCities.Count);
     }
 
     [Theory]
     [MemberData(nameof(CityRepositoryTestData.CityRepositoryValidTestData),
         MemberType = typeof(CityRepositoryTestData))]
-    [Trait("Category", "Queries")]
-    public async Task GetByIdAsync_ShouldReturnCityWithMatchingId(City cityToFind)
+    [Trait("Category", "City")]
+    public async Task GetByIdAsync_ShouldReturnCityWithMatchingId_WhenCityWithMatchingIdExists(City cityToFind)
     {
-        // Arrange
-        _context.Cities.Add(cityToFind);
-        await _context.SaveChangesAsync();
+        await AddCity(cityToFind);
 
         var sut = new CityRepository(_context);
 
-        // Act
         var result = await sut.GetByIdAsync(cityToFind.Id);
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(cityToFind.Id, result?.Id);
-        Assert.Equal(cityToFind.Name, result?.Name);
+        
+        result.Should().NotBeNull();
+        result?.Id.Should().Be(cityToFind.Id);
+        result?.Name.Should().Be(cityToFind.Name);
     }
 
     [Theory]
     [MemberData(nameof(CityRepositoryTestData.CityRepositoryValidTestData),
         MemberType = typeof(CityRepositoryTestData))]
-    [Trait("Category", "Commands")]
-    public async Task InsertAsync_ShouldInsertCity(City cityToAdd)
+    [Trait("Category", "City")]
+    public async Task InsertAsync_ShouldInsertCity_WhenCityIsInserted(City cityToAdd)
     {
-        // Arrange
         var sut = new CityRepository(_context);
 
-        // Act
         var result = await sut.InsertAsync(cityToAdd);
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.NotEqual(Guid.Empty, result?.Id);
-        Assert.Equal(cityToAdd.Name, result?.Name);
+        result.Should().NotBeNull();
+        result?.Id.Should().NotBe(Guid.Empty);
+        result?.Name.Should().Be(cityToAdd.Name);
 
-        // Check if the city is actually in the database
-        var savedCity = await _context.Cities.FindAsync(result?.Id);
-        Assert.NotNull(savedCity);
-        Assert.Equal(cityToAdd.Name, savedCity?.Name);
+        var savedCity = await _context
+            .Cities
+            .FindAsync(result?.Id);
+        
+        savedCity.Should().NotBeNull();
+        savedCity?.Name.Should().Be(cityToAdd.Name);
     }
 
     [Theory]
     [MemberData(nameof(CityRepositoryTestData.CityRepositoryValidTestData),
         MemberType = typeof(CityRepositoryTestData))]
-    [Trait("Category", "Commands")]
-    public async Task UpdateAsync_ShouldUpdateCity(City existingCity)
+    [Trait("Category", "City")]
+    public async Task UpdateAsync_ShouldUpdateCity_WhenCityIsUpdated(City existingCity)
     {
-        // Arrange
-        _context.Cities.Add(existingCity);
-        await _context.SaveChangesAsync();
-        _context.Entry(existingCity).State = EntityState.Detached;
+        await AddCity(existingCity);
 
         var updatedCity = new City
         {
@@ -103,44 +88,41 @@ public class CityRepositoryTests
             PostOffice = "PostOffice"
         };
         var sut = new CityRepository(_context);
-
-        // Act
+        
         await sut.UpdateAsync(updatedCity);
 
-        // Assert
-        var result = await _context.Cities.FindAsync(existingCity.Id);
-        Assert.NotNull(result);
-        Assert.Equal(updatedCity.Name, result?.Name);
+        var result = await _context
+            .Cities
+            .FindAsync(existingCity.Id);
+        
+        result.Should().NotBeNull();
+        result?.Name.Should().Be(updatedCity.Name);
     }
 
     [Theory]
     [MemberData(nameof(CityRepositoryTestData.CityRepositoryValidTestData),
         MemberType = typeof(CityRepositoryTestData))]
-    [Trait("Category", "Commands")]
-    public async Task DeleteAsync_ShouldDeleteCity(City cityToDelete)
+    [Trait("Category", "City")]
+    public async Task DeleteAsync_ShouldDeleteCity_WhenCityIsDeleted(City cityToDelete)
     {
-        // Arrange
-        await _context.Cities.AddAsync(cityToDelete);
-        await _context.SaveChangesAsync();
-        _context.Entry(cityToDelete).State = EntityState.Detached;
-
+        await AddCity(cityToDelete);
         var sut = new CityRepository(_context);
+        
+        await sut.DeleteAsync(cityToDelete.Id);
+        
+        var result = await _context
+            .Cities
+            .AsNoTracking()
+            .SingleOrDefaultAsync
+            (city => cityToDelete.Id.Equals(city.Id));
 
-        // Act
-        var cityExistsBeforeDeletion = await _context.Cities.AnyAsync(city => city.Id == cityToDelete.Id);
+        result.Should().BeNull();
+    }
 
-        if (cityExistsBeforeDeletion)
-        {
-            await sut.DeleteAsync(cityToDelete.Id);
-        }
-        else
-        {
-            _testOutputHelper.WriteLine($"City with ID {cityToDelete.Id} does not exist. Skipping deletion.");
-        }
-
-        // Assert
-        _testOutputHelper.WriteLine(cityToDelete.Id.ToString());
-        var result = await _context.Cities.AsNoTracking().SingleOrDefaultAsync(city => cityToDelete.Id.Equals(city.Id));
-        Assert.Null(result);
+    private async Task AddCity(City city)
+    {
+        await _context.Cities.AddAsync(city);
+        await _context.SaveChangesAsync();
+        _context.Entry(city).State = EntityState.Detached;
     }
 }
