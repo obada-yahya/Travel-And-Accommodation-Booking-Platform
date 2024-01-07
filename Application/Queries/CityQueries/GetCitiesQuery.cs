0ca5fc0f9f886a9c6d11196bca003cef94169289
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-07 15:18:22 +0200

    Added GetCitiesQuery, GetCityByIdQuery

---
+++ Application/Queries/CityQueries/GetCitiesQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.CityDtos;
+using Domain.Common;
+using MediatR;
+
+namespace Application.Queries.CityQueries;
+
+public record GetCitiesQuery : IRequest<(IReadOnlyList<CityDto>, PaginationMetaData)>
+{
+    public bool IncludeHotels { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
