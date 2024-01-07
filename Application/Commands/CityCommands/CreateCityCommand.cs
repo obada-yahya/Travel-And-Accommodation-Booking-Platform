ce7abd3b8734282c90595534bd5bf5f9ed3e97b4
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-07 15:20:45 +0200

    Add CityCommands : Create, Delete, Update

---
+++ Application/Commands/CityCommands/CreateCityCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.CityDtos;
+using MediatR;
+
+namespace Application.Commands.CityCommands;
+
+public record CreateCityCommand : IRequest<CityWithoutHotelsDto?>
+{
+    public string Name { get; set; } = string.Empty;
+    public string CountryName { get; set; } = string.Empty;
+    public string CountryCode { get; set; } = string.Empty;
+    public string PostOffice { get; set; } = string.Empty;
+}
---
+++ Application/Commands/CityCommands/UpdateCItyCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Application.Commands.CityCommands;
+
+public record UpdateCityCommand: IRequest
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string CountryName { get; set; } = string.Empty;
+    public string CountryCode { get; set; } = string.Empty;
+    public string PostOffice { get; set; } = string.Empty;
+}
