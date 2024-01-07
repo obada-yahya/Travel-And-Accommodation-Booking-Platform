a0fb5d79f65d57b88f442fcb4dd73b386c67be85
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-07 15:12:27 +0200

    Updated ICityRepository

--- Domain/Common/Interfaces/ICityRepository.cs
+++ Domain/Common/Interfaces/ICityRepository.cs
@@ -1,11 +1,11 @@
 ﻿using Domain.Entities;
-
 namespace Domain.Common.Interfaces;
 
 public interface ICityRepository
 {
-    public Task<IReadOnlyList<City>> GetAllAsync();
-    public Task<City?> GetByIdAsync(Guid cityId);
+    public Task<(IReadOnlyList<City>, PaginationMetaData)>
+    GetAllAsync(bool includeHotels, int pageNumber, int pageSize);
+    public Task<City?> GetByIdAsync(Guid cityId, bool includeHotels);
     Task<City?> InsertAsync(City city);
     Task UpdateAsync(City city);
     Task DeleteAsync(Guid cityId);
