ebcd678d31abba3d2b4bc3c06c485a8d8d9c8dca
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-07 15:19:54 +0200

    Added HotelDtos

---
+++ Application/DTOs/HotelDtos/HotelDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+namespace Application.DTOs.HotelDtos;
+
+public class HotelDto
+{
+    public Guid Id { get; set; }
+    public Guid CityId { get; set; }
+    public Guid OwnerId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public float Rating { get; set; }
+    public string StreetAddress { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public int FloorsNumber { get; set; }
+    public IList<Room> Rooms { get; set; }
+}
