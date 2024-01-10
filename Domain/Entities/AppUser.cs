e8fa32d114193cd35d9e51357e3ba457b7ea704e
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-10 16:26:38 +0200

    Added appUser Entity

---
+++ Domain/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class AppUser
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+    public string Role { get; set; }
+}
