ff64abe86fe41a50ea7811310cb5fd17a72733b2
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-17 22:51:05 +0200

    Added AuthUser For Authentication Service

---
+++ Infrastructure/Auth/AuthUser/AuthUser.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Auth.Models;
+using Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Auth.AuthUser;
+
+public class AuthUser : IAuthUser
+{
+    private readonly ApplicationDbContext _context;
+
+    public AuthUser(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetUserAsync(string email)
+    {
+        var appUser = await _context
+            .AppUsers
+            .SingleOrDefaultAsync(appUser => appUser.Email.Equals(email));
+
+        if (appUser is null) return null;
+        
+        return new User(
+            appUser.Email,
+            appUser.PasswordHash,
+            appUser.Role,
+            appUser.Salt);
+    }
+}
--- Infrastructure/ServiceCollectionExtensions.cs
+++ Infrastructure/ServiceCollectionExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Domain.Common.Interfaces;
+using Infrastructure.Auth;
+using Infrastructure.Auth.AuthUser;
 using Infrastructure.Common.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using PasswordHashing;
 
 namespace Infrastructure;
 
@@ -9,6 +12,10 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddTransient<ICityRepository, CityRepository>();
+        services.AddTransient<IAppUserRepository, AppUserRepository>();
+        services.AddTransient<ITokenGenerator, JwtTokenGenerator>();
+        services.AddTransient<IPasswordGenerator, Argon2PasswordGenerator>();
+        services.AddTransient<IAuthUser, AuthUser>();
         return services;
     }
 }
