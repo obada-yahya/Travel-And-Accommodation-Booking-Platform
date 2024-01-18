b422f4bcd51f357ce6788738f4d4dc24f70dc720
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-18 11:48:27 +0200

    Added Validators for Authentication

---
+++ TAABP.API/Validators/AuthValidators/RegisterRequestBodyValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs.AppUserDtos;
+using FluentValidation;
+
+namespace TAABP.API.Validators.AuthValidators;
+
+public class RegisterRequestBodyValidator : GenericValidator<AppUserForCreationDto>
+{
+    public RegisterRequestBodyValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email address.");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Must(password => password.Any(char.IsUpper) && password.Any(char.IsLower))
+            .WithMessage("Password must contain both uppercase and lowercase characters.")
+            .Must(password => password.Any(char.IsDigit))
+            .WithMessage("Password must contain at least one digit.")
+            .Must(password => password.Any(ch => !char.IsLetterOrDigit(ch)))
+            .WithMessage("Password must contain at least one special character.");
+    }
+}
