afb75deb1f72c315aa3097f464f4c93236d491d3
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-22 22:28:58 +0200

    Adding Image Entity

---
+++ Domain/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Image
+{
+    public Guid Id { get; set; }
+    public Guid EntityId { get; set; }
+    public string Url { get; set; }
+    public ImageFormat Format { get; set; }
+}
