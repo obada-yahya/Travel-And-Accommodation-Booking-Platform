756f8c4fc500684d0f2265d1d3c19c6e1d4dafa4
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-22 22:30:13 +0200

    Added CheckCityExistsQuery

---
+++ Application/Handlers/CityHandlers/CheckCityExistsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.CityQueries;
+using Domain.Common.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.CityHandlers;
+
+public class CheckCityExistsQueryHandler : IRequestHandler<CheckCityExistsQuery, bool>
+{
+    private readonly ICityRepository _cityRepository;
+
+    public CheckCityExistsQueryHandler(ICityRepository cityRepository)
+    {
+        _cityRepository = cityRepository;
+    }
+    
+    public async Task<bool> Handle(CheckCityExistsQuery request, CancellationToken cancellationToken)
+    {
+        return await _cityRepository.IsExistsAsync(request.Id);
+    }
+}
---
+++ Application/Queries/CityQueries/CheckCityExistsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Queries.CityQueries;
+
+public class CheckCityExistsQuery : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
