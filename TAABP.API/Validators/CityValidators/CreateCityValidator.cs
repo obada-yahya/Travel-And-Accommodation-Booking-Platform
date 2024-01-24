6074e2fdab016a841d63a4dc692fd7aa5601982b
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-24 18:10:31 +0200

    City Validators

---
+++ TAABP.API/Validators/CityValidators/CreateCityValidator.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTOs.CityDtos;
+using FluentValidation;
+
+namespace TAABP.API.Validators.CityValidators;
+
+public class CreateCityValidator : GenericValidator<CityForCreationDto>
+{
+    public CreateCityValidator()
+    {
+        RuleFor(city => city.Name)
+            .NotEmpty()
+            .WithMessage("Name field shouldn't be empty");
+
+        RuleFor(city => city.CountryCode)
+            .NotEmpty()
+            .Must(city => city.Length == 3)
+            .WithMessage("CountryCode must be exactly 3 characters");
+        
+        RuleFor(city => city.PostOffice)
+            .NotEmpty()
+            .WithMessage("PostOffice field shouldn't be empty");
+        
+        RuleFor(city => city.CountryName)
+            .NotEmpty()
+            .WithMessage("CountryName field shouldn't be empty");
+    }
+}
