33d5c9c93b68837c92fc0dbb5fcec0dc79839d16
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-25 09:57:38 +0200

    Added RoomAmentiy Repository

---
+++ Domain/Common/Interfaces/IRoomAmenityRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common.Models;
+using Domain.Entities;
+
+namespace Domain.Common.Interfaces;
+
+public interface IRoomAmenityRepository
+{
+    public Task<PaginatedList<RoomAmenity>>
+        GetAllAsync(int pageNumber, int pageSize);
+    public Task<RoomAmenity?> GetByIdAsync(Guid amenityId);
+    public Task<RoomAmenity?> InsertAsync(RoomAmenity roomAmenity);
+    public Task UpdateAsync(RoomAmenity roomAmenity);
+    public Task DeleteAsync(Guid amenityId);
+    public Task SaveChangesAsync();
+    public Task<bool> IsExistsAsync(Guid amenityId);
+}
