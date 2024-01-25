ebc3fa6182260eaf690069e76d746e3bb61bec97
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-25 09:55:37 +0200

    Added RoomAmenity

---
+++ Domain/Entities/RoomAmenity.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class RoomAmenity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public IList<RoomType> RoomTypes { get; set; }
+}
