ea17e90be13c9fad089849f47619f395afd83079
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-25 12:31:46 +0200

    Added RoomAmenities Dtos

---
+++ Application/DTOs/RoomAmenityDtos/RoomAmenityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.RoomAmenityDtos;
+
+public class RoomAmenityDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ Application/DTOs/RoomAmenityDtos/RoomAmenityForCreationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTOs.RoomAmenityDtos;
+
+public class RoomAmenityForCreationDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
