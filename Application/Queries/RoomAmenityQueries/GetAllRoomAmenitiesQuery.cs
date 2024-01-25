4ee64a9c1f023cbc15ca3e292cdde2b727e4b5e2
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-25 12:34:59 +0200

    Added ل RoomAmenity Queries

---
+++ Application/Queries/RoomAmenityQueries/GetAllRoomAmenitiesQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs.RoomAmenityDtos;
+using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Queries.RoomAmenityQueries;
+
+public record GetAllRoomAmenitiesQuery : IRequest<PaginatedList<RoomAmenityDto>>
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
---
+++ Application/Queries/RoomAmenityQueries/GetRoomAmenityByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.RoomAmenityDtos;
+using MediatR;
+
+namespace Application.Queries.RoomAmenityQueries;
+
+public record GetRoomAmenityByIdQuery : IRequest<RoomAmenityDto?>
+{
+    public Guid Id { get; set; }
+}
