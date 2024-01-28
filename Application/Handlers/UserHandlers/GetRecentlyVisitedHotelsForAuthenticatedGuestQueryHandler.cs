983eb244b0358185b5e6aa5285134cc8a4383c21
Obada-Yahya <obadayahya.an@gmail.com>
2024-01-28 15:32:04 +0200

    Added Guest Queries Handlers

---
+++ Application/Handlers/UserHandlers/GetRecentlyVisitedHotelsForAuthenticatedGuestQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.DTOs.HotelDtos;
+using Application.Queries.UserQueries;
+using AutoMapper;
+using Domain.Common.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.UserHandlers;
+
+public class GetRecentlyVisitedHotelsForAuthenticatedGuestQueryHandler :
+    IRequestHandler<GetRecentlyVisitedHotelsForAuthenticatedGuestQuery, List<HotelWithoutRoomsDto>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetRecentlyVisitedHotelsForAuthenticatedGuestQueryHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<HotelWithoutRoomsDto>> Handle(GetRecentlyVisitedHotelsForAuthenticatedGuestQuery request,
+        CancellationToken cancellationToken)
+    {
+        return _mapper.Map<List<HotelWithoutRoomsDto>>
+        (await _userRepository
+        .GetRecentlyVisitedHotelsForAuthenticatedGuest
+        (request.Email, request.Count));
+    }
+}
