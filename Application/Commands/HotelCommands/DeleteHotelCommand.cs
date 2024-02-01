f4604d39538abe8da08429942121f2fe3d768c4b
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-01 21:42:32 +0200

    Added hotel commands and queries

---
+++ Application/Commands/HotelCommands/DeleteHotelCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Commands.HotelCommands;
+
+public record DeleteHotelCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ Application/Queries/HotelQueries/GetAllHotelsQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.HotelDtos;
+using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Queries.HotelQueries;
+
+public record GetAllHotelsQuery : IRequest<PaginatedList<HotelWithoutRoomsDto>>
+{
+    public string? SearchQuery { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
