bc6d034506799887815c5e83ec296f8de1733cc7
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-01 12:16:52 +0200

    Added Validators for booking, reserve

---
+++ TAABP.API/Validators/BookingValidators/BookingsQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.BookingQueries;
+using FluentValidation;
+
+namespace TAABP.API.Validators.BookingValidators;
+
+public class BookingsQueryValidator : GenericValidator<GetBookingsByHotelIdQuery>
+{
+    public BookingsQueryValidator()
+    {
+        RuleFor(booking => booking.PageNumber)
+            .GreaterThan(0)
+            .WithMessage("Page number must be greater than 0.");
+
+        RuleFor(booking => booking.PageSize)
+            .GreaterThan(0)
+            .WithMessage("Page size must be greater than 0.")
+            .LessThan(21)
+            .WithMessage("Page Size can't be greater than 20");
+    }
+}
