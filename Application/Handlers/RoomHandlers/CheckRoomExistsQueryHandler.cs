0206354b26c9b372891b3bf85afc5812e72df046
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-01 12:01:54 +0200

    Added room queries and handlers

---
+++ Application/Handlers/RoomHandlers/CheckRoomExistsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.RoomQueries;
+using Domain.Common.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.RoomHandlers;
+
+public class CheckRoomExistsQueryHandler : IRequestHandler<CheckRoomExistsQuery, bool>
+{
+    private readonly IRoomRepository _roomRepository;
+
+    public CheckRoomExistsQueryHandler(IRoomRepository roomRepository)
+    {
+        _roomRepository = roomRepository;
+    }
+
+    public async Task<bool> Handle(CheckRoomExistsQuery request, CancellationToken cancellationToken)
+    {
+        return await _roomRepository.IsExistsAsync(request.Id);
+    }
+}
