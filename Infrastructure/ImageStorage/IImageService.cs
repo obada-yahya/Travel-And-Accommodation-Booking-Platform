d8a39f5ab02dcad0456f43683fe8109d3bad3f2b
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-03 09:29:54 +0200

    Updated ImageService

--- Infrastructure/ImageStorage/IImageService.cs
+++ Infrastructure/ImageStorage/IImageService.cs
@@ -6,4 +6,5 @@ public interface IImageService
 {
     public Task UploadImageAsync(ImageCreationDto imageCreationDto);
     public Task<List<string>> GetAllImagesAsync(Guid entityId);
+    public Task DeleteImageAsync(Guid entityId, Guid imageId);
 }
