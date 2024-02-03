33d943257458d0739c6aea0deafa970d3ab8b187
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-03 09:30:37 +0200

    Added CheckHotelExistsQueryHandler

---
+++ Application/Handlers/HotelHandlers/CheckHotelExistsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Queries.HotelQueries;
+using Domain.Common.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.HotelHandlers;
+
+public class CheckHotelExistsQueryHandler : IRequestHandler<CheckHotelExistsQuery,bool>
+{
+    private readonly IHotelRepository _hotelRepository;
+
+    public CheckHotelExistsQueryHandler(IHotelRepository hotelRepository)
+    {
+        _hotelRepository = hotelRepository;
+    }
+    
+    public async Task<bool> Handle(CheckHotelExistsQuery request, CancellationToken cancellationToken)
+    {
+        return await _hotelRepository.IsExistsAsync(request.Id);
+    }
+}
