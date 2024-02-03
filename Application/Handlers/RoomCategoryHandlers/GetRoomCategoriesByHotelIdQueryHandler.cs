64756ea2db84260783348b40b75f841fd38c45a0
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-03 18:24:36 +0200

    Updated city, roomCategory repositories

---
+++ Application/Handlers/RoomCategoryHandlers/GetRoomCategoriesByHotelIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.DTOs.RoomCategoriesDtos;
+using Application.Queries.RoomCategoryQueries;
+using AutoMapper;
+using Domain.Common.Interfaces;
+using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Handlers.RoomCategoryHandlers;
+
+public class GetRoomCategoriesByHotelIdQueryHandler :
+IRequestHandler<GetRoomCategoriesByHotelIdQuery,PaginatedList<RoomTypeDto>>
+{
+    private readonly IRoomTypeRepository _roomTypeRepository;
+    private readonly IMapper _mapper;
+
+    public GetRoomCategoriesByHotelIdQueryHandler(IRoomTypeRepository roomTypeRepository, IMapper mapper)
+    {
+        _roomTypeRepository = roomTypeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<RoomTypeDto>> Handle(GetRoomCategoriesByHotelIdQuery request, CancellationToken cancellationToken)
+    {
+        var paginatedList = await
+            _roomTypeRepository
+                .GetAllAsync(
+                request.HotelId,
+                request.IncludeAmenities,
+                request.PageNumber,
+                request.PageSize
+                );
+
+        return new PaginatedList<RoomTypeDto>(
+            _mapper.Map<List<RoomTypeDto>>(paginatedList.Items),
+            paginatedList.PageData);
+    }
+}
--- Domain/Common/Interfaces/IRoomTypeRepository.cs
+++ Domain/Common/Interfaces/IRoomTypeRepository.cs
@@ -1,10 +1,16 @@
-﻿using Domain.Entities;
+﻿using Domain.Common.Models;
+using Domain.Entities;
 
 namespace Domain.Common.Interfaces;
 
 public interface IRoomTypeRepository
 {
-    public Task<IReadOnlyList<RoomType>> GetAllAsync();
+    public Task<PaginatedList<RoomType>> 
+    GetAllAsync(
+        Guid hotelId,
+        bool includeAmenities,
+        int pageNumber,
+        int pageSize);
     public Task<RoomType?> GetByIdAsync(Guid roomTypeId);
     Task<RoomType?> InsertAsync(RoomType roomType);
     Task UpdateAsync(RoomType roomType);
