bb9821cf9886207d769b429eeb72bad72ea4ed67
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-09 09:24:53 +0200

    Added TrendingCities Query Handler

---
+++ Application/Queries/CityQueries/GetTrendingCitiesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.CityDtos;
+using MediatR;
+
+namespace Application.Queries.CityQueries;
+
+public record GetTrendingCitiesQuery : IRequest<List<CityWithoutHotelsDto>>
+{
+    public int Count { get; set; } = 5;
+}
