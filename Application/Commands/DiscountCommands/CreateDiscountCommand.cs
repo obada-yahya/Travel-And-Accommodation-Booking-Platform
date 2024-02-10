8404a31107da65cdabc7ba493a2c38885627c90d
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-10 10:45:09 +0200

    Added Create,Delete discount commands

---
+++ Application/Commands/DiscountCommands/CreateDiscountCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.DiscountDtos;
+using MediatR;
+
+namespace Application.Commands.DiscountCommands;
+
+public record CreateDiscountCommand : IRequest<DiscountDto?>
+{
+    public Guid RoomTypeId { get; set; }
+    public float DiscountPercentage { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+}
---
+++ Application/Commands/DiscountCommands/DeleteDiscountCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Commands.DiscountCommands;
+
+public record DeleteDiscountCommand : IRequest
+{
+    public Guid Id { get; set; }
+}
