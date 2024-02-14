36d4988fe0446a5c4f734f816d10c3b04ced4492
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-14 10:21:51 +0200

    Added home controller

---
+++ TAABP.API/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+using Application.Queries.CityQueries;
+using Application.Queries.HotelQueries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TAABP.API.Controllers;
+
+[ApiController]
+[Route("api/home")]
+public class HomeController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public HomeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Retrieves the top 5 trending cities.
+    /// </summary>
+    /// <returns>The top 5 trending cities.</returns>
+    [HttpGet("destinations/trending")]
+    [Authorize]
+    public async Task<IActionResult> GetTrendingCities()
+    {
+        var request = new GetTrendingCitiesQuery();
+        return Ok(await _mediator.Send(request));
+    }
+
+    /// <summary>
+    /// Searches for hotels based on the provided search query.
+    /// </summary>
+    /// <param name="query">The hotel search query.</param>
+    /// <returns>A list of hotels matching the search criteria.</returns>
+    [HttpGet("search")]
+    [Authorize]
+    public async Task<IActionResult> Search([FromQuery] HotelSearchQuery query)
+    {
+        var result = await _mediator.Send(query);
+        
+        Response.Headers.Add("X-Pagination",
+            JsonSerializer.Serialize(result.PageData));
+        
+        return Ok(result.Items);
+    }
+    
+    /// <summary>
+    /// Retrieves the top 5 featured hotel deals.
+    /// </summary>
+    /// <returns>The top 5 featured hotel deals.</returns>
+    [HttpGet("featured-deals")]
+    [Authorize]
+    public async Task<IActionResult> FeaturedDeals()
+    {
+        return Ok(await _mediator.Send(new GetFeaturedDealsQuery{Count = 5}));
+    }
+}
