a3453736c804c9d9918009af42a3f8d123d33b0f
Obada-Yahya <obadayahya.an@gmail.com>
2024-02-16 23:25:10 +0200

    Added Pdf Service

---
+++ Infrastructure/Pdf/PdfService.cs
@@ -0,0 +1,12 @@
+﻿using NReco.PdfGenerator;
+
+namespace Infrastructure.Pdf;
+
+public class PdfService : IPdfService
+{
+    public async Task<byte[]> CreatePdfFromHtmlAsync(string html)
+    {
+        var htmlToPdfConverter = new HtmlToPdfConverter();
+        return await Task.FromResult(htmlToPdfConverter.GeneratePdf(html));
+    }
+}
